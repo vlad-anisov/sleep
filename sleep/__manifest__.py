884007c2e2e318612e1665238e0a3a8942b252b0
vlad <anisoffgo@gmail.com>
2024-07-08 13:28:39 +0300

    Init

---
+++ sleep/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Sleep",
+    "version": "17.0",
+    "category": "",
+    "summary": "Summary",
+    "description": """ Description """,
+    "depends": [
+        "base",
+    ],
+    "data": [
+    ],
+
+    "application": True,
+}
